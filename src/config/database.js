7cfeb7bf5e615303cdd5e378cbc8c8528fc1cc79
Victor <vramh.developer@gmail.com>
2020-04-10 14:33:34 -0400

    se cambio direccion mlab de nuevo

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'url': 'mongodb+srv://vramhdev:torvic2114@cluster0-3wbqs.mongodb.net/prodb?retryWrites=true&w=majority'
+    'url': 'mongodb+srv://vramhdev:torvic2114@cluster0-3wbqs.mongodb.net/test?retryWrites=true&w=majority'
     
 };
