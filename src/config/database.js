05a28e3dc0ab48403ded23e4ebf122574c4ccd12
Victor <vramh.developer@gmail.com>
2020-04-11 09:52:08 -0400

    Se agrega cambio de nuevo

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'url': 'mongodb://vramhdev:torvic2114@cluster0-3wbqs.mongodb.net:27017/prodb?retryWrites=true&w=majority'
+    'url': 'mongodb://vramhdev:torvic2114@cluster0-3wbqs.mongodb.net/prodb?retryWrites=true&w=majority'
     
     
 };
