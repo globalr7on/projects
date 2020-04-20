5c8fa546c75e7e3abb392f6144ade9a4f9eefa5e
Victor <vramh.developer@gmail.com>
2020-04-20 10:54:07 -0400

    update in server

--- src/server.js
+++ src/server.js
@@ -11,36 +11,6 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const { url } = require('./config/database');
 
-// hacer storage para imagenes
-const storage = multer.diskStorage({
-    destination: './storage/imgs/',
-    filename: function(_req, file, cb){
-        cb(null, file.fieldname + '-' + Date.now() +
-        path.extname(file.originalname));
-    }
-})
-
-//inicializar storage
-const upload = multer({
-    storage: storage,
-    limits:{fileSize: 1000000},
-    fileFilter: function(_req, file, cb){
-        checkFileType(file, cb);
-    }
-}).single('person.imgUrl');
-
-//check file Type
-function checkFileType(file, cb){
-    const filetypes= /jpeg|jpg|png|gif/;
-    const extname = filetypes.test(path.extname
-        (file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-    if(mimetype && extname){
-        return cb(null, true);
-    }else {
-        cb('Error: Imagenes solamente ')
-    }
-}
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -71,6 +41,36 @@ app.use(flash());
 
 // routes 
 
+// hacer storage para imagenes
+const storage = multer.diskStorage({
+    destination: './storage/imgs/',
+    filename: function(_req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() +
+        path.extname(file.originalname));
+    }
+})
+
+//inicializar storage
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function(_req, file, cb){
+        checkFileType(file, cb);
+    }
+}).single('person.imgUrl');
+
+//check file Type
+function checkFileType(file, cb){
+    const filetypes= /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname
+        (file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if(mimetype && extname){
+        return cb(null, true);
+    }else {
+        cb('Error: Imagenes solamente ')
+    }
+}
 
 
 //base routes
